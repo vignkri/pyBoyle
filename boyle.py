fba80ca6da361b61f1b5a4a12bfcb2ef9dc33e39
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-16 22:10:34 +0200

    Set up initial values for solver.

--- boyle.py
+++ boyle.py
@@ -16,3 +16,9 @@ yield_c = np.loadtxt("./sample/yc", comments="%")
 regulate = np.loadtxt("./sample/regulate", comments="%")
 const1 = np.loadtxt("./sample/Const1", comments="%")
 const2 = np.loadtxt("./sample/Const2", comments="%")
+
+# Set up initial values
+volume = initial[0]
+substrate = initial[1:20]
+degraders = initial[20:29]
+gas_conc = np.zeros((4,))
