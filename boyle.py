6f76eb4ebbc0871591353852e635b9ebb2c67abe
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-16 22:06:53 +0200

    Load sample data files.

---
+++ boyle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import numpy as np
+
+
+"""
+Boyle Python
+
+Simulation agent for Biogas Production
+"""
+
+
+# Import datasets
+initial = np.loadtxt("./sample/Initial", comments="%")
+yield_c = np.loadtxt("./sample/yc", comments="%")
+regulate = np.loadtxt("./sample/regulate", comments="%")
+const1 = np.loadtxt("./sample/Const1", comments="%")
+const2 = np.loadtxt("./sample/Const2", comments="%")
