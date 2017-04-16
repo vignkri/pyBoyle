4f0e3d90f8bf2dfc5e514804ae842e9131fac6fb
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-16 22:57:06 +0200

    Expand docstring of standard model to handle basic parameter list.

--- model.py
+++ model.py
@@ -8,4 +8,17 @@ Standard Computation Model
 
 
 def standard(initial, time):
-    """Standard Integrator Model"""
+    """Standard Integrator Model
+
+    PARAMETERS
+    ----------
+    init : list
+        A list of four values:
+            - volume
+            - substrates : numpy.array
+            - degraders  : numpy.array
+            - gas_values : numpy.array
+
+    OTHER PARAMETERS
+    ----------------
+    """
