eca4dab33f9ebc43d4c8b642c06f3c74feff59dd
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-17 00:04:40 +0200

    Pass flow and yieldc to compute gasflow.

--- boyle.py
+++ boyle.py
@@ -143,4 +143,5 @@ initial_values = np.concatenate((np.array([volume]), substrate,
 solution = scipy.integrate.odeint(model.standard, y0=initial_values,
                                   t=time_array,
                                   args=(loggers, constants_one, mu_max, xxval,
-                                        mu_max_t0, [k0_carbon, k0_prot]))
+                                        mu_max_t0, [k0_carbon, k0_prot],
+                                        [flow_in, flow_out], yield_c))
