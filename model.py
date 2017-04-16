d569a044f0d9ea3a7f9b6cf91ecc94aacba97c1a
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-16 23:28:05 +0200

    Set up initial values for computations and additional argument for constant_one variables.

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ Standard Computation Model
 
 
 def standard(initial, time,
-             logging_headers):
+             logging_headers, constant_ones):
     """Standard Integrator Model
 
     PARAMETERS
@@ -41,3 +41,10 @@ def standard(initial, time,
     with open("./logging/degraders.log", "a") as degrlog:
         degrlogwriter = csv.DictWriter(degrlog, fieldnames=degrheader)
         degrlogwriter.writerow(degrvalues)
+
+    # Initial Chemical Concentrations
+    carbo_is, carbo_in, carbon, lipids, lcfa, \
+        prot_is, prot_in, amino, nh3, hac, hpr, hbut, hval, \
+        ch4, co2, h2s, z, h2po4, a = substrate
+    dead_cells = degraders[0]
+    degraders = degraders[1:]
