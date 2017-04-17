Correct the timespan computation and remove critical point space.

boyle.py
@@ -137,7 +137,8 @@ constants_one = [ks, ks_nh3, pk_low, pk_high, ks_nh3,
 xxval = [k_h, ka_nh4, ka_hac, ka_hpr, ka_hbut, ka_hval, ka1_co2,
          ka2_co2, ka_h2s, ka_h2po4, kw]
 # Set up integrator
-time_array = np.linspace(start_time, end_time, end_time*2)
+time_array = np.linspace(start_time, end_time,
+                         (end_time - start_time)/step_size)
 initial_values = np.concatenate((np.array([volume]), substrate,
                                  degraders, gas_conc))
 solution = scipy.integrate.odeint(model.standard, y0=initial_values,
@@ -146,4 +147,4 @@ solution = scipy.integrate.odeint(model.standard, y0=initial_values,
                                         mu_max_t0, [k0_carbon, k0_prot],
                                         [flow_in, flow_out], yield_c,
                                         substrate_inflow),
-                                  tcrit=np.arange(0, len(time_array), 1))
+                                  full_output=True)