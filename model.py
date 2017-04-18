3021c2f4cf53f443e48e0b81fd328c0b655f48af
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-18 18:55:58 +0200

    FIX: Take conjugate of yieldc before transposing for the multiplication.

--- model.py
+++ model.py
@@ -141,7 +141,7 @@ def standard(time, y0,
     flow_in, flow_out = flow
     y_dot = np.zeros((33,))
     y_dot[0] = flow_in - flow_out
-    y_dot[1:17] = (yieldc.transpose().dot(z))
+    y_dot[1:17] = (yieldc.conj().transpose().dot(z))
     y_dot[20] = np.sum(cell_death) - cell_decay
     y_dot[21:29] = z[3:] - cell_death.reshape(-1)
     y_dot[1:29] = y_dot[1:29] + (inflow - flow_in * y0[1:29]) / volume
