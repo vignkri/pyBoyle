5b89135838a830672cee53dbfe27404efeb0d815
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-19 22:47:55 +0200

    Create header lists for creating files.

--- simlog.py
+++ simlog.py
@@ -15,3 +15,22 @@ class Simulog:
     def __init__(self, path):
         """Initialize data logging folder path."""
         self._path = path
+        # Create internally useful headers
+        self.__headers = {
+            "mu": ["time", "mu_one", "mu_two", "mu_three", "mu_four",
+                   "mu_five", "mu_six", "mu_seven", "mu_eight", "afterflag"],
+            "substrates": ["time", "volume", "carbo_is", "carbo_in", "carbon",
+                           "lipids", "lcfa", "prot_is", "prot_in", "amino",
+                           "nh3", "hac", "hpr", "hbut", "hval", "ch4",
+                           "co2", "h2s", "zplus", "h2po4", "aminus"],
+            "degraders": ["time", "carb_degr", "amino_degr", "lipid_degr",
+                          "lcfa_degr", "prop_degr", "butyr_degr",
+                          "valer_degr", "acet_degr"],
+            "result": ["time", "volume", "carbois", "carboin", "carbon",
+                       "lipids", "lcfa", "protis", "protin", "amino",
+                       "nh3", "hac", "hpr", "hbut", "hval", "ch4",
+                       "co2", "h2s", "zplus", "h2po4", "aminus", "deadcell",
+                       "carb_degr", "amino_degr", "lipid_degr", "lcfa_degr",
+                       "prop_degr", "butyr_degr", "valer_degr", "acet_degr",
+                       "gfnh3", "gfch4", "gfco2", "gfh2s"]
+        }
