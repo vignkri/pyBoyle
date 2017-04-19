64f2376d0f6b1f06ce38632cb72d3f5fe5471175
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-04-19 22:20:25 +0200

    pH Computation framework added to the standard model.

--- model.py
+++ model.py
@@ -71,6 +71,33 @@ def standard(time, y0,
 
     # Set up pH Computation
     H = 1e-8
+    Hfunc = 1
+    # --
+    while abs(Hfunc - H) > 1e-12:
+        Hfunc = co2 / 44 * ka1_co2 * (H + 2 * ka2_co2) / \
+            (H * (H + ka1_co2) + ka1_co2 * ka2_co2) + \
+            hac / 60 * ka_hac / (H + ka_hac) + \
+            hpr / 74 * ka_hpr / (H + ka_hpr) + \
+            hbut / 88 * ka_hbut / (H + ka_hbut) + \
+            hval / 102 * ka_hval / (H + ka_hval) + \
+            a / 35.5 + \
+            h2po4 / 31 * (H + 2 * ka_h2po4) / (H + ka_h2po4) - \
+            nh3 / 14 * H / (H + ka_nh4) - \
+            z / 39 + \
+            kw / H
+        # --
+        dhfunc_dh = - co2 / 44 * ka1_co2 * (H * (H + 4 * ka2_co2) + ka1_co2 *
+                                            ka2_co2) / \
+            (H * (H + ka1_co2) + ka1_co2 * ka2_co2)**2 - \
+            hac / 60 * ka_hac / (H + ka_hac)**2 - \
+            hpr / 74 * ka_hpr / (H + ka_hpr)**2 - \
+            hbut / 88 * ka_hbut / (H + ka_hbut)**2 - \
+            hval / 102 * ka_hval / (H + ka_hval)**2 - \
+            kw / (H * H) - \
+            h2po4 / 31 * ka_h2po4 / (H + ka_h2po4)**2 - \
+            nh3 / 14 * ka_nh4 / (H + ka_nh4)**2
+        H = H - (Hfunc - H) / (dhfunc_dh - 1)
+    # --
     pH = -np.log10(H)
 
     # Calculation of growth rates
