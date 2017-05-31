4c03a3fdca2c4fbe7f12f3eba043a5a76ba8552c
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-05-31 22:00:43 +0200

    MODEL: Create recursive pH function to handle the computation of pH using newton-raphson.

--- boyle/model.py
+++ boyle/model.py
@@ -7,6 +7,54 @@ Standard Computation Model
 """
 
 
+def newton_pH(H, Hfunc, i=0, **kwargs):
+    """pH Computation using Newton-Raphson method"""
+    # -- set up variables
+    co2, ka1_co2, ka2_co2 = kwargs.get("co2")
+    hac, ka_hac = kwargs.get("HAc")
+    hpr, ka_hpr = kwargs.get("HPr")
+    hbut, ka_hbut = kwargs.get("HBut")
+    hval, ka_hval = kwargs.get("HVal")
+    a, z, kw = kwargs.get("Other")
+    h2po4, ka_h2po4 = kwargs.get("h2po4")
+    nh3, ka_nh4 = kwargs.get("NH3")
+    # --
+    Hfunc = co2 / 44 * ka1_co2 * (H + 2 * ka2_co2) / \
+        (H * (H + ka1_co2) + ka1_co2 * ka2_co2) + \
+        hac / 60 * ka_hac / (H + ka_hac) + \
+        hpr / 74 * ka_hpr / (H + ka_hpr) + \
+        hbut / 88 * ka_hbut / (H + ka_hbut) + \
+        hval / 102 * ka_hval / (H + ka_hval) + \
+        a / 35.5 + \
+        h2po4 / 31 * (H + 2 * ka_h2po4) / (H + ka_h2po4) - \
+        nh3 / 14 * H / (H + ka_nh4) - \
+        z / 39 + \
+        kw / H
+    # --
+    dhfunc_dh = - co2 / 44 * ka1_co2 * (H * (H + 4 * ka2_co2) + ka1_co2 *
+                                        ka2_co2) / \
+        (H * (H + ka1_co2) + ka1_co2 * ka2_co2)**2 - \
+        hac / 60 * ka_hac / (H + ka_hac)**2 - \
+        hpr / 74 * ka_hpr / (H + ka_hpr)**2 - \
+        hbut / 88 * ka_hbut / (H + ka_hbut)**2 - \
+        hval / 102 * ka_hval / (H + ka_hval)**2 - \
+        kw / (H * H) - \
+        h2po4 / 31 * ka_h2po4 / (H + ka_h2po4)**2 - \
+        nh3 / 14 * ka_nh4 / (H + ka_nh4)**2
+    # --
+    H = H - (Hfunc - H) / (dhfunc_dh - 1)
+    # --
+    pH = -np.log10(H)
+    # --
+    if abs(Hfunc - H) > 1e-12 and pH is None:
+        new_args = kwargs
+        H = H
+        Hfunc = Hfunc
+        newton_pH(H, Hfunc, i=i, **new_args)
+    else:
+        return pH
+
+
 def standard(time, y0,
              constant_ones, mu_max, xxval, mu_max_t0,
              k0_zeros, flow, yieldc, inflow, simlog):
@@ -60,36 +108,16 @@ def standard(time, y0,
     #       Section 2: pH Computation
     #
     # ---------------------------------------------------
-
     H = 1e-8
     Hfunc = 1
-    # --
-    while abs(Hfunc - H) > 1e-12:
-        Hfunc = co2 / 44 * ka1_co2 * (H + 2 * ka2_co2) / \
-            (H * (H + ka1_co2) + ka1_co2 * ka2_co2) + \
-            hac / 60 * ka_hac / (H + ka_hac) + \
-            hpr / 74 * ka_hpr / (H + ka_hpr) + \
-            hbut / 88 * ka_hbut / (H + ka_hbut) + \
-            hval / 102 * ka_hval / (H + ka_hval) + \
-            a / 35.5 + \
-            h2po4 / 31 * (H + 2 * ka_h2po4) / (H + ka_h2po4) - \
-            nh3 / 14 * H / (H + ka_nh4) - \
-            z / 39 + \
-            kw / H
-        # --
-        dhfunc_dh = - co2 / 44 * ka1_co2 * (H * (H + 4 * ka2_co2) + ka1_co2 *
-                                            ka2_co2) / \
-            (H * (H + ka1_co2) + ka1_co2 * ka2_co2)**2 - \
-            hac / 60 * ka_hac / (H + ka_hac)**2 - \
-            hpr / 74 * ka_hpr / (H + ka_hpr)**2 - \
-            hbut / 88 * ka_hbut / (H + ka_hbut)**2 - \
-            hval / 102 * ka_hval / (H + ka_hval)**2 - \
-            kw / (H * H) - \
-            h2po4 / 31 * ka_h2po4 / (H + ka_h2po4)**2 - \
-            nh3 / 14 * ka_nh4 / (H + ka_nh4)**2
-        H = H - (Hfunc - H) / (dhfunc_dh - 1)
-    # --
-    pH = -np.log10(H)
+
+    # -- call pH computation function using newton-raphson
+    # -- this function is recursive and therefore additional information
+    # -- and logging should be added to get best data possible
+    pH = newton_pH(H, Hfunc, co2=[co2, ka1_co2, ka2_co2],
+                   HAc=[hac, ka_hac], HPr=[hpr, ka_hpr], HBut=[hbut, ka_hbut],
+                   HVal=[hval, ka_hval], Other=[a, z, kw],
+                   h2po4=[h2po4, ka_h2po4], NH3=[nh3, ka_nh4])
 
     # ---------------------------------------------------
     #
