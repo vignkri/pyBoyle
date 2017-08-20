9fea199ed225ae498f8d47acab256f6b66b99cd5
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-08-20 17:02:05 +0200

    Include missing variables to the constants_one dictionary.

--- boyle/frame.py
+++ boyle/frame.py
@@ -65,7 +65,8 @@ class Frame:
                 pk_low=const1[2:, 9], pk_high=const1[2:, 10],
                 ki_carbon=const1[0, 7], ki_prot=const1[1, 7],
                 ki_hac_hpr=const1[6, 7], ki_hac_hbut=const1[7, 7],
-                ki_nh3_hac=const1[9, 8], ki_lcfa=const1[2:, 8])
+                ki_nh3_hac=const1[9, 8], ki_hac_hval=const1[8, 7],
+                ki_lcfa=const1[2:, 8])
         ))
         # -- mu_max parameters
         self.mu_max.update(dict(
