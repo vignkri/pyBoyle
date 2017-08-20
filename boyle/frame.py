c6952b4ccccd59a1383eb649d6415fc60f4ebe51
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-08-20 11:29:24 +0200

    Frame scaffold for importing and preprocessing data.

---
+++ boyle/frame.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+
+"""
+Process Frame data
+
+The process data for the simulation is imported
+and the data is cleaned up and updated to run the
+simulation.
+"""
+
+
+class Frame:
+    def __init__(self, folder):
+        """Set up Frame for setting up process information"""
