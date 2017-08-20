113d6dd848b816215862625b55933f1248314b58
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-08-20 14:31:07 +0200

    Set up a frame class which imports data and sets it as attribute.

--- boyle/frame.py
+++ boyle/frame.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env
 
+import os
+import numpy as np
+
 """
 Process Frame data
 
@@ -12,3 +15,21 @@ simulation.
 class Frame:
     def __init__(self, folder):
         """Set up Frame for setting up process information"""
+        try:
+            assert os.path.exists(folder)
+            self._folder = folder
+        except AssertionError as e:
+            print("Folder not found.", e)
+        else:
+            items = list(os.walk(folder))
+            fldr, lst, files = items[0]
+            _names = [item for item in files]
+            _files = [os.path.join(fldr, item) for item in files]
+            self.import_files(names=_names, files=_files)
+
+    def import_files(self, names, files):
+        """Import the files from the defined folder"""
+        parsed = tuple(zip(names, files))
+        for name, _file in parsed:
+            setattr(self, name, {"path": _file,
+                                 "value": np.loadtxt(_file, comments="%")})
