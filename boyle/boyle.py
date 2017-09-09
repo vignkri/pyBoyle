ea60d3bbd5b2946497f14067d99240a2a1e6f9ad
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2017-09-09 15:48:50 +0200

    BOYLE: Remove all settings and configuration definitions.

--- boyle/boyle.py
+++ boyle/boyle.py
@@ -23,14 +23,6 @@ solver_settings = configuration.get("solver")
 regulate_settings = configuration.get("regulate")
 boyle_logger.info("Configuration file loaded.")
 
-# Simulation settings
-data_folder = configuration.get("data")
-experiment_name = configuration.get("name")
-start_time = settings.get("t_initial")
-end_time = settings.get("t_final")
-step_size = settings.get("step_size")
-
-
 # Regulate Settings
 temp = regulate_settings.get("temperature")
 flow_in = regulate_settings.get("flow_in")
@@ -48,11 +40,7 @@ boyle_logger.info("Input data loaded.")
 # --
 boyle_logger.info("Finished setting up constants.")
 
-_config = dict(initial=dataset.Initial.get("value"),
-               start_time=start_time, end_time=end_time,
-               step=step_size, metadata=experiment_name)
-
-solver = Manager(model.standard, config=_config)
+solver = Manager(model.standard, config=dataset._simulation_config)
 solver.initialize_solver(iname="vode", i_params=dataset._solver)
 solver.function_parameters(parameters=[dataset])
 solver.start()
