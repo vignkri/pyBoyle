f06f0d07d61bf0d3e18a1b0045f94b2c2e94ff35
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-05-07 20:02:03 +0200

    Flexible PH method choice by switch case.

--- boyle/model.py
+++ boyle/model.py
@@ -63,7 +63,7 @@ def newton_pH(H, Hfunc, i=0, **kwargs):
     #     return H
 
 
-def standard(time, y0, dataset, output, run_no):
+def standard(time, y0, dataset, output, run_no, ph_mode="standard"):
     """Standard Integrator Model
 
     PARAMETERS
@@ -142,13 +142,16 @@ def standard(time, y0, dataset, output, run_no):
     Hfunc = 1
     pH = 8
 
-    while abs(Hfunc - H) > 1e-12 or pH is None:
-        H, Hfunc = newton_pH(H, Hfunc, co2=[co2, ka1_co2, ka2_co2],
-                             HAc=[hac, ka_hac], HPr=[hpr, ka_hpr],
-                             HBut=[hbut, ka_hbut],
-                             HVal=[hval, ka_hval], Other=[a, z, kw],
-                             h2po4=[h2po4, ka_h2po4], NH3=[nh3, ka_nh4])
-        pH = - np.log10(Hfunc)
+    if ph_mode == "standard":
+        while abs(Hfunc - H) > 1e-12 or pH is None:
+            H, Hfunc = newton_pH(H, Hfunc, co2=[co2, ka1_co2, ka2_co2],
+                                 HAc=[hac, ka_hac], HPr=[hpr, ka_hpr],
+                                 HBut=[hbut, ka_hbut],
+                                 HVal=[hval, ka_hval], Other=[a, z, kw],
+                                 h2po4=[h2po4, ka_h2po4], NH3=[nh3, ka_nh4])
+            pH = - np.log10(Hfunc)
+    else:
+        print("Unknown method")
 
     # -- call pH computation function using newton-raphson
     # -- this function is recursive and therefore additional information
