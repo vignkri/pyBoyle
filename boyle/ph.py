a30fcc15a800f0d6ac40aef5707adba704656ac7
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-05-10 23:12:52 +0200

    PH: Add code for computing pH using fsolve and brentq methods.

--- boyle/ph.py
+++ boyle/ph.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env/python
 
 
+from scipy.optimize import fsolve, brentq
+
 """
 pH computation
 
@@ -50,3 +52,28 @@ def newton_raphson(H, Hfunc, i=0, **kwargs):
     # --
     H = H - (Hfunc - H) / (dhfunc_dh - 1)
     return H, Hfunc
+
+
+def calculate(H, *args, **kwargs):
+    # -- set up variables
+    co2, ka1_co2, ka2_co2 = kwargs.get("co2")
+    hac, ka_hac = kwargs.get("HAc")
+    hpr, ka_hpr = kwargs.get("HPr")
+    hbut, ka_hbut = kwargs.get("HBut")
+    hval, ka_hval = kwargs.get("HVal")
+    a, z, kw = kwargs.get("Other")
+    h2po4, ka_h2po4 = kwargs.get("h2po4")
+    nh3, ka_nh4 = kwargs.get("NH3")
+    # --
+    Hfunc = co2 / 44 * ka1_co2 * (H + 2 * ka2_co2) / (H * (H + ka1_co2) +
+                                                      ka1_co2 * ka2_co2) + \
+        hac / 60 * ka_hac / (H + ka_hac) + \
+        hpr / 74 * ka_hpr / (H + ka_hpr) + \
+        hbut / 88 * ka_hbut / (H + ka_hbut) + \
+        hval / 102 * ka_hval / (H + ka_hval) + \
+        a / 35.5 + \
+        h2po4 / 31 * (H + 2 * ka_h2po4) / (H + ka_h2po4) - \
+        nh3 / 14 * H / (H + ka_nh4) - \
+        z / 39 + \
+        kw / H
+    return H - Hfunc
