1ae2a1fc727b77d64342bb1429b2474bf5fb9074
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-05-11 22:13:00 +0200

    IO: Fix debug_solution link for experiment status checks.

--- boyle/iotools.py
+++ boyle/iotools.py
@@ -263,7 +263,7 @@ class io:
             out_grp["solution"] = self.solution[1:]
             # -- Deprecate this:
             if self.__experiment_status == "debug":
-                out_grp["debug_solution"] = self.dbgSoln[1:]
+                out_grp["debug_solution"] = self.debug_solution[1:]
             else:
                 pass
             # --
