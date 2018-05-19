00aa2bc508223fc1285688b1b25a55fedf35743b
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-05-19 11:12:09 +0200

    IO: ki_nh3_hac value should point to the correct column index.

--- boyle/iotools.py
+++ boyle/iotools.py
@@ -198,7 +198,7 @@ class io:
             pk_low=const1[2:, 9], pk_high=const1[2:, 10],
             ki_carbon=const1[0, 7], ki_prot=const1[1, 7],
             ki_hac_hpr=const1[6, 7], ki_hac_hbut=const1[7, 7],
-            ki_nh3_hac=const1[9, 8], ki_hac_hval=const1[8, 7],
+            ki_nh3_hac=const1[9, 7], ki_hac_hval=const1[8, 7],
             ki_lcfa=const1[2:, 8]
         )
         self.Const1.update(dict(params=const_one_payload))
