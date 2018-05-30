849e5dac198d5ac7a059e5ab8e7d31a55639cbaa
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-05-30 22:27:10 +0200

    [tests] add preliminary tests for comparing input data.

---
+++ tests/test_boyle.py
@@ -0,0 +1,37 @@
+from boyle import io
+from boyle import __version__
+
+
+def test_version():
+    assert __version__ == '0.5.0'
+
+
+def checkConstants(ds, raw):
+    assert (ds == raw).all()
+
+
+def test_initialisation():
+    """Simulate settings of the testing folder."""
+    import numpy as np
+    # --
+    data_path = "data/"
+    # -- Create sample dataset
+    _metadata_ = {"name": "Pytesting Data",
+                  "data": data_path, "description": "test description",
+                  "tags": "sample, tag, setup"}
+    _settings_ = {"process": "debug",
+                  "constants": "standard", "step_size": 0.5,
+                  "ph": {"method": "fixed"},
+                  "solver": {"method": "bdf", "order": 1,
+                             "nsteps": 2, "relative": 1e-4,
+                             "absolute": 1e-8}}
+    # --
+    _config_ = {"metadata": _metadata_, "settings": _settings_}
+    # --
+    dataset = io(_config_)
+    # --
+    const_one = np.loadtxt("data/Const1.constant", comments="%")
+    const_two = np.loadtxt("data/Const2.constant", comments="%")
+    # print(dataset.Const1.get("value"))
+    checkConstants(ds=dataset.Const1.get("value"), raw=const_one)
+    checkConstants(ds=dataset.Const2.get("value"), raw=const_two)
