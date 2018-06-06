b5b99ff99969f2793b23ba331a6c07bee93bb15b
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-06-06 23:41:38 +0200

    [api] open the functions from the save module.

--- boyle/__init__.py
+++ boyle/__init__.py
@@ -3,6 +3,7 @@ __version__ = "0.5.0"
 # Set up imports
 
 from boyle.core.generic import Dataset
+from boyle.core import save
 from boyle.manager import Manager
 
 # Set up imports from api in submodules
