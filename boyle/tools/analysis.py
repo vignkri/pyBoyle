0ea270dd9d3da8b6367d3baca3d53b37277b0efd
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2018-06-17 21:26:43 +0200

    [analysis] add function to help compute BMP from the concentration.

--- boyle/tools/analysis.py
+++ boyle/tools/analysis.py
@@ -19,3 +19,12 @@ def interpolateData(data, time_step=1):
     tarr = np.arange(start_time, end_time, time_step)
     out_ = interpolate.griddata(time, data, tarr, method="linear")
     return out_
+
+
+def computeBMP(arr, multiplier=None):
+    """Compute BMP from the concentration data"""
+    if not multiplier:
+        multiplier = np.array([0.5*373.5, 373.5, 0.8*572.6, 572.6,
+                               1014, 1014, 530, 636.6, 714, 373.5])
+    # --
+    return (arr) @ (multiplier)
